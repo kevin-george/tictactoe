4b8f07f05c11d7a4e4da993d6d416bdefa8cedbf
Travis Chung <Travis@wg-dhcp201d201d213.secure.wireless.fsu.edu>
2015-07-14 21:58:18 -0400

    rearranged start_client

--- server/server_child.c
+++ server/server_child.c
@@ -12,127 +12,128 @@
 
 void print_help(int fd) {
   char full_string[1500];
-  char *str = full_string;
-  str += sprintf(str, "\nCommands supported:\n"); 
-  str += sprintf(str, "  %-24s # List all online users\n", "who");
-  str += sprintf(str, "  %-24s # Display user information\n", "stats");
-  str += sprintf(str, "  %-24s # List all current games\n", "game");
-  str += sprintf(str, "  %-24s # Observe a game\n", "observe <game_num>");
-  str += sprintf(str, "  %-24s # Unobserve a game\n", "unobserve");
-  str += sprintf(str, "  %-24s # Try to start a game\n", "match <name> <b|w> [t]");
-  str += sprintf(str, "  %-24s # Make a move in a game\n", "<A|B|C<1|2|3>");
-  str += sprintf(str, "  %-24s # Resign a game\n", "resign");
-  str += sprintf(str, "  %-24s # Refresh a game\n", "refresh");   
-  str += sprintf(str, "  %-24s # Shout <msg> to every one online\n", "shout <msg>");
-  str += sprintf(str, "  %-24s # Tell user <name> message\n", "tell <name> <msg>");
+    char *str = full_string;
+    str += sprintf(str, "\nCommands supported:\n"); 
+    str += sprintf(str, "  %-24s # List all online users\n", "who");
+    str += sprintf(str, "  %-24s # Display user information\n", "stats");
+    str += sprintf(str, "  %-24s # List all current games\n", "game");
+    str += sprintf(str, "  %-24s # Observe a game\n", "observe <game_num>");
+    str += sprintf(str, "  %-24s # Unobserve a game\n", "unobserve");
+    str += sprintf(str, "  %-24s # Try to start a game\n", "match <name> <b|w> [t]");
+    str += sprintf(str, "  %-24s # Make a move in a game\n", "<A|B|C<1|2|3>");
+    str += sprintf(str, "  %-24s # Resign a game\n", "resign");
+    str += sprintf(str, "  %-24s # Refresh a game\n", "refresh");   
+    str += sprintf(str, "  %-24s # Shout <msg> to every one online\n", "shout <msg>");
+    str += sprintf(str, "  %-24s # Tell user <name> message\n", "tell <name> <msg>");
   str += sprintf(str, "  %-24s # Comment on a game when observing\n", "kibitz <msg>");
-  str += sprintf(str, "  %-24s # Comment on a game\n", "' <msg>");
-  str += sprintf(str, "  %-24s # Quiet mode, no broadcast messages\n", "quiet");
-  str += sprintf(str, "  %-24s # Non-quiet mode\n", "nonquiet");
-  str += sprintf(str, "  %-24s # No more communication from <id>\n", "block <id>");
-  str += sprintf(str, "  %-24s # Allow communication from <id>\n", "unblock <id>");
-  str += sprintf(str, "  %-24s # List the header of the mails\n", "listmail");
-  str += sprintf(str, "  %-24s # Read the particular mail\n", "readmail <msg_num>");
-  str += sprintf(str, "  %-24s # Delete the particular mail\n", "deletemail <msg_num>");
-  str += sprintf(str, "  %-24s # Send id a mail\n", "mail <id> <title>");
-  str += sprintf(str, "  %-24s # Change your information to <msg>\n", "info <msg>");
-  str += sprintf(str, "  %-24s # Change password\n", "passwd <new>");
-  str += sprintf(str, "  %-24s # Quit the system\n", "exit");
-  str += sprintf(str, "  %-24s # Quit the system\n", "quit");
-  str += sprintf(str, "  %-24s # Print this message\n", "help");
-  str += sprintf(str, "  %-24s # Print this message\n", "?");
-  my_write(fd, full_string, strlen(full_string));
+    str += sprintf(str, "  %-24s # Comment on a game\n", "' <msg>");
+    str += sprintf(str, "  %-24s # Quiet mode, no broadcast messages\n", "quiet");
+    str += sprintf(str, "  %-24s # Non-quiet mode\n", "nonquiet");
+    str += sprintf(str, "  %-24s # No more communication from <id>\n", "block <id>");
+    str += sprintf(str, "  %-24s # Allow communication from <id>\n", "unblock <id>");
+    str += sprintf(str, "  %-24s # List the header of the mails\n", "listmail");
+    str += sprintf(str, "  %-24s # Read the particular mail\n", "readmail <msg_num>");
+    str += sprintf(str, "  %-24s # Delete the particular mail\n", "deletemail <msg_num>");
+    str += sprintf(str, "  %-24s # Send id a mail\n", "mail <id> <title>");
+    str += sprintf(str, "  %-24s # Change your information to <msg>\n", "info <msg>");
+    str += sprintf(str, "  %-24s # Change password\n", "passwd <new>");
+    str += sprintf(str, "  %-24s # Quit the system\n", "exit");
+    str += sprintf(str, "  %-24s # Quit the system\n", "quit");
+    str += sprintf(str, "  %-24s # Print this message\n", "help");
+    str += sprintf(str, "  %-24s # Print this message\n", "?");
+    my_write(fd, full_string, strlen(full_string));
 }
 
-void run_command(char* command, int fd) {
+void run_command(char* command, int clientfd) {
   char message[50];
-  char user_id[USERNAME_LENGTH], passwd[50];
+    char user_id[USERNAME_LENGTH], passwd[50];
+    printf ("command:%s\n", command);
   
-  if(starts_with(command, "register")) {
-    char* token = strtok(command, " ");
-    int counter = 0;
-    while(token != NULL) {
-      token = strtok(NULL, " ");
-      if(counter == 0)
-        strcpy(user_id, token);
-      else
-        strcpy(passwd, token);
-      counter++;
+    if(starts_with(command, "register")) {
+      char* token = strtok(command, " ");
+      int counter = 0;
+      while(token != NULL) {
+          token = strtok(NULL, " ");
+          if(counter == 0)
+            strcpy(user_id, token);
+          else
+            strcpy(passwd, token);
+          counter++;
+      }
+      if(register_user(user_id, passwd) != 0) {
+          my_error("Registration Failed!");
+          strcpy(message, "\nRegistration Failed!");
+          my_write(clientfd, message, strlen(message));
+      } else {
+          strcpy(message, "\nRegistration Completed!");
+          my_write(clientfd, message, strlen(message));
+      }
+    } else if(strcmp(command, "who") == 0) {
+      //To be implemented
+    } else if(strcmp(command, "help") == 0) {
+      print_help(clientfd);
+    } else if (strcmp(command, "tell") == 0) {
+        // Check if user exists
+
+            // Send parent message, 
+
+        // If user doesn't exist, output message
+    }else {
+      my_error("Unsuported command!");
+      strcpy(message, "\nUnsupported Command!");
+      my_write(clientfd, message, strlen(message));
     }
-    if(register_user(user_id, passwd) != 0) {
-      my_error("Registration Failed!");
-      strcpy(message, "\nRegistration Failed!");
-      my_write(fd, message, strlen(message));
-    } else {
-      strcpy(message, "\nRegistration Completed!");
-      my_write(fd, message, strlen(message));
-    }
-  } else if(strcmp(command, "who") == 0) {
-    //To be implemented
-  } else if(strcmp(command, "help") == 0) {
-    print_help(fd);
-  } else {
-    my_error("Unsuported command!");
-    strcpy(message, "\nUnsupported Command!");
-    my_write(fd, message, strlen(message));
-  }
 }
 
 void start_client(int id, int listenfd, struct sockaddr_in cliaddr, 
                                           int *p2c_fd, int *c2p_fd) {
-  char cmd[CMD_LENGTH];
-  int cli_sock;
-  char message[100];
-  int command_counter = 0;
-  char user_id[50], passwd[50];
+    char cmd[CMD_LENGTH];
+    int cli_sock;
+    char username[USERNMAE_LENGTH];
+    char message[100];
+    int command_counter = 0;
+    char user_id[50], passwd[50];
 
-  socklen_t clilen = sizeof(cliaddr);
-  cli_sock = my_accept(listenfd, (struct sockaddr *)(&cliaddr), &clilen); 
+    socklen_t clilen = sizeof(cliaddr);
 
-  printf("Client connected to child %d <machine = %s, port = %x, %x.>\n", 
-      id, inet_ntoa(cliaddr.sin_addr), cliaddr.sin_port, ntohs(cliaddr.sin_port));
+    while(true) {
+        // Idle till connection is made
+        cli_sock = my_accept(listenfd, (struct sockaddr *)(&cliaddr), &clilen); 
+        printf("Client connected to child %d <machine = %s, port = %x, %x.>\n", 
+            id, inet_ntoa(cliaddr.sin_addr), cliaddr.sin_port, ntohs(cliaddr.sin_port));
 
-  char fresh_login = 'Y';
-  do{
-    //First present with a guest login prompt if user_id is
-    //not set
-    if(*(get_userid() + 0) == '\0') {
-      my_write(cli_sock, "\n\n\nusername(guest):", 19);
-      my_read(cli_sock, cmd, CMD_LENGTH);
-      //If user didn't enter anything, we give them a guest user_id
-      if(cmd[0] == '\0') {
-        print_help(cli_sock);
-        fresh_login = 'N';
-        strcpy(message, "\nYou login as a guest.The only command that you can use is\n'register <username> <password>'");
-        my_write(cli_sock, message, strlen(message));
-        set_userid("guest"); 
-      } else {
-        //The user is trying to login, authenticate who they are
-        if(authenticate_user(user_id, passwd) == 0)
-          set_userid(user_id);
-      }
-      continue;
-    } else {
-      //So user_id is set, based on their type allow them to run commands
-      if(fresh_login == 'Y') {
-        print_help(cli_sock);
-        fresh_login = 'N';
-      }
-      sprintf(message, "\n\n<%s: %d> ", get_userid(), command_counter);
-      my_write(cli_sock, message, strlen(message));
-      my_read(cli_sock, cmd, CMD_LENGTH);
-      if(strcmp(get_userid(), "guest") == 0) {
-        if(starts_with(cmd, "register") != 0 && (strcmp(cmd, "exit") != 0 
-                                                  && strcmp(cmd, "quit") != 0)) {
-          strcpy(message, "\nYou are not supposed to do this.\nYou can only use 'register <username> <password>' as guest.");
-          my_write(cli_sock, message, strlen(message));
+        // Login
+        my_write(cli_sock, "\n\n\nusername(guest):", 19);
+        my_read(cli_sock, cmd, CMD_LENGTH);
+        //If user didn't enter anything, we give them a guest user_id
+        if(cmd[0] == '\0') {
+            print_help(cli_sock);
+            strcpy(message, "\nyou login as a guest.the only command that you can use is\n'register <username> <password>'");
+            my_write(cli_sock, message, strlen(message));
+            set_userid("guest");
+        } else {
+            // The user is trying to login, authenticate who they are
+            if (authenticate_user(user_id, passwd) == 0)
+                set_userid(user_id);
         }
-      } else {
-      run_command(cmd, cli_sock);
-      }
-    }
-    command_counter++;
-  } while(strcmp(cmd, "exit") != 0 && strcmp(cmd, "quit") != 0);
 
-  close(cli_sock);  
-}
+        print_help(cli_sock);
+
+        do {  // Game loop
+          sprintf(message, "\n\n<%s: %d> ", get_userid(), command_counter);
+          my_write(cli_sock, message, strlen(message));
+          my_read(cli_sock, cmd, CMD_LENGTH);
+          // user_id is set, based on their type allow them to run commands
+          if (strcmp(get_userid, "guest") == 0) {
+            if (starts_with(cmd, "register") != 0 && (strcmp(cmd, "exit") != 0 
+                                                  && strcmp(cmd, "quit") != 0)) {
+              strcpy(message, "\nYou are not supposed to do this.\nYou can only use 'register <username> <password>' as guest.");
+                my_write(cli_sock, message, strlen(message));
+            }
+          } else {
+            run_command(cmd, cli_sock, p2c_fd, c2p_fd);
+          }
+
+          ++command_counter;
+        } while (strcmp(cmd, "exit") != 0 && strcmp(cmd, "quit") != 0);
+    }
--- server/server_parent.c
+++ server/server_parent.c
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <netinet/in.h>
@@ -18,68 +19,94 @@
 int child_id = 0;
 
 void sig_chld(int signo) {
-  pid_t pid;
-  int stat;
-  while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) 
-    printf("child %d terminated.\n", (int)pid);
-  return ;
+    pid_t pid;
+    int stat;
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) 
+        printf("child %d terminated.\n", (int)pid);
 }
 
 int main(int argc, char * argv[]) {
-  int listenfd;
-  int cpid;
-  struct sockaddr_in addr, recaddr;
-  struct sigaction abc;
-  int p2c_fd[20][2], c2p_fd[20][2];
+    int listenfd, maxr_fd, maxr_newcli_fd;
+    int cpid;
+    char buf[100];
+    char **client_names;
+    struct sockaddr_in addr, recaddr;
+    struct sigaction abc;
+    int p2c_fd[20][2], c2p_fd[20][2],  c2p_newcli_fd[20][2];
+    fd_set rset_newcli, rset;
+    struct timeval tv;
 
-  abc.sa_handler = sig_chld;
-  sigemptyset(&abc.sa_mask);
-  abc.sa_flags = 0;
+    tv.tv_sec = 0;
+    FD_ZERO(&rset);
+    FD_ZERO(&rset_newcli);
 
-  sigaction(SIGCHLD, &abc, NULL);
+    abc.sa_handler = sig_chld;
+    sigemptyset(&abc.sa_mask);
+    abc.sa_flags = 0;
 
-  if(argc < 2) {
-    my_log("Usage: ./server <port number>.\n");
-  }
+    sigaction(SIGCHLD, &abc, NULL);
 
-  listenfd = my_socket(AF_INET, SOCK_STREAM, 0);
-
-  addr.sin_addr.s_addr = INADDR_ANY;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons((short)atoi(argv[1]));
-
-  my_bind(listenfd, (struct sockaddr *)&addr, sizeof(addr));
-
-  unsigned int len = sizeof(addr);
-  getsockname(listenfd, (struct sockaddr *)&addr, &len);
-
-  printf("\nListening on port %s...\n", argv[1]);
-
-  my_listen(listenfd, 5);
-
-  for(int id = 0; id < CLIENT_SIZE; ++id) {
-    if((cpid = fork()) == 0) {
-      //Why are we skipping id = 0?
-      if(id >= 1) {
-        //Close existing child pipes
-        for(int i = 0; i < id; ++i) {
-          close(p2c_fd[i][0]); close(c2p_fd[i][1]);
-          close(c2p_fd[i][0]); close(c2p_fd[i][1]);
-        }
-      }
-      pipe(p2c_fd[id]); pipe(c2p_fd[id]);
-      start_client(id, listenfd, recaddr, p2c_fd[id], c2p_fd[id]);
-      exit(0);
-    } else if(cpid < 0) {
-      my_error("fork failed");
-    } else {
-      close(p2c_fd[id][0]); close(c2p_fd[id][1]);
+    if(argc < 2) {
+        my_log("Usage: ./server <port number>.\n");
     }
-  }
 
-  for ( ; ; ) {
+    client_names = (char**)malloc(sizeof(char*) * CLIENT_SIZE);
 
-  }
+    listenfd = my_socket(AF_INET, SOCK_STREAM, 0);
+
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons((short)atoi(argv[1]));
+
+    my_bind(listenfd, (struct sockaddr *)&addr, sizeof(addr));
+
+    unsigned int len = sizeof(addr);
+    getsockname(listenfd, (struct sockaddr *)&addr, &len);
+
+    printf("\nListening on port %s...\n", argv[1]);
+
+    my_listen(listenfd, 5);
+
+    for(int id = 0; id < CLIENT_SIZE; ++id) {
+        pipe(p2c_fd[id]); 
+        pipe(c2p_fd[id]);
+        pipe(c2p_newcli_fd[id]);
+        if((cpid = fork()) == 0) {
+            if(id >= 1) {
+                //Close existing child pipes
+                for(int i = 0; i < id; ++i) {
+                    close(p2c_fd[i][0]); close(c2p_fd[i][1]);
+                    close(c2p_fd[i][0]); close(c2p_fd[i][1]);
+                    close (c2p_newcli_fd[i][0]); close (c2p_newcli_fd[i][1]);
+                }
+            }
+            start_client(id, listenfd, recaddr, p2c_fd[id], c2p_fd[id]);
+            exit(0);
+        } else if(cpid < 0) {
+            my_error("fork failed");
+        } else {
+            close(p2c_fd[id][0]); close(c2p_fd[id][1]);
+            close (c2p_newcli_fd[id][1]);
+            FD_SET(c2p_fd[id][0], &rset);
+            FD_SET(c2p_newcli_fd[id][0], &rset_newcli);
+            maxr_fd = c2p_fd[id][0] + 1;
+            maxr_newcli_fd = c2p_newcli_fd[id][0] + 1;
+        }
+    }
+
+    for ( ; ; ) {
+        //my_select(maxr_fd, &rset, NULL, NULL, &tv);
+        my_select(maxr_newcli_fd, &rset_newcli, NULL, NULL, &tv);
+
+        for (int i = 0; i < NEW_CLIENT; ++i) {
+            if (FD_ISSET(c2p_newcli_fd[i][0], &rset_newcli)) {
+                my_read(c2p_newcli_fd[i][0], buf, sizeof(buf));
+                client_names[i] = (char*)malloc(sizeof(char)*strlen(buf));
+                strcpy(client_names[i], buf);
+                printf("client_names[%d]: %s\n", i, client_names[i]);
+            }
+        }
+    }
 }
 
 
